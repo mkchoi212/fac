7456d0a07fe50a47a30fff35af82ee099766df74
Mike JS. Choi <mkchoi212@icloud.com>
2017-12-31 14:33:58 -0600

    Fix diff newline error

--- diff.go
+++ diff.go
@@ -8,6 +8,6 @@ import (
 
 func (c *Conflict) diff() []string {
 	dmp := diffmatchpatch.New()
-	diffs := dmp.DiffMain(strings.Join(c.CurrentLines, "\n"), strings.Join(c.ForeignLines, "\n"), false)
+	diffs := dmp.DiffMain(strings.Join(c.CurrentLines, ""), strings.Join(c.ForeignLines, ""), false)
 	return []string{dmp.DiffPrettyText(diffs)}
 }
