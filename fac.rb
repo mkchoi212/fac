53749d8ce814d94ae155725d645aa81e3dbb258b
Mike Choi <mkchoi212@icloud.com>
2018-01-02 19:18:46 -0600

    fac version v1.0.1

--- fac.rb
+++ fac.rb
@@ -3,7 +3,7 @@ class Fac < Formula
   homepage "https://github.com/mkchoi212/fac"
   url "https://github.com/mkchoi212/fac/releases/download/v1.0.1/fac_1.0.1_darwin_amd64.tar.gz"
   version "1.0.1"
-  sha256 "74e28c0400a71e4cc2bf3f0b54a74d3e98cdcedb048b36445fbf55d42fa7c8ca"
+  sha256 "f64e25dae39e9eb160afe225754b345e850489f7dcab707bc513311d7098d59a"
   
   depends_on "git"
   depends_on "go"
