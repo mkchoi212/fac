7d8c39e2cd9d60bf8bc7bc36f58e7250ddaebad5
Mike Choi <mkchoi212@icloud.com>
2018-01-05 22:53:55 -0600

    fac version v1.0.3

--- Formula/fac.rb
+++ Formula/fac.rb
@@ -1,9 +1,9 @@
 class Fac < Formula
   desc "Command line User Interface for fixing git conflicts"
   homepage "https://github.com/mkchoi212/fac"
-  url "https://github.com/mkchoi212/fac/releases/download/v1.0.2/fac_1.0.2_darwin_amd64.tar.gz"
-  version "1.0.2"
-  sha256 "d5369b5a8532b8be25b061f32ace7531868e361258141f6ac44932006eb4e451"
+  url "https://github.com/mkchoi212/fac/releases/download/v1.0.3/fac_1.0.3_darwin_amd64.tar.gz"
+  version "1.0.3"
+  sha256 "a1ee8ed558fba5eeef74068a7666ec4c1b28f809ecd31e44055152c177dbe1b3"
   
   depends_on "git"
   depends_on "go"
