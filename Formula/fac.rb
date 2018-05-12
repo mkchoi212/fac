e3cff7e3bb1043dd624ca2583bfa5bc87f293b81
Mike Choi <mkchoi212@icloud.com>
2018-05-11 19:49:43 -0500

    Brew formula update for fac version v1.1.0

--- Formula/fac.rb
+++ Formula/fac.rb
@@ -1,18 +1,13 @@
 class Fac < Formula
   desc "Command line User Interface for fixing git conflicts"
   homepage "https://github.com/mkchoi212/fac"
-  url "https://github.com/mkchoi212/fac/releases/download/v1.0.4/fac_1.0.4_darwin_amd64.tar.gz"
-  version "1.0.4"
-  sha256 "5aaab92e82e02594126f54ab973b2c9eaab422ac13b87d7c79fe7f1afdc42965"
+  url "https://github.com/mkchoi212/fac/releases/download/v1.1.0/fac_1.1.0_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "01e72c6147ea5d5cfb10f9c95e13a27ba764b7e71cdd409a96fd566ee16fbc4c"
   
   depends_on "git"
-  depends_on "go"
 
   def install
     bin.install "fac"
-  end
-
-  test do
-    
   end
 end
